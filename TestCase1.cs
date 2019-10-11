1640601a9d9e6880c95ce0988ce650187509209f
Zafar Ahmad <ZAhmad@pattersoncompanies.com>
2019-10-11 18:44:11 +0500

    First Master File

---
+++ TestCase1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    class TestCase1
+    {
+        static void Main(string[] args)
+        { }
+          public void Login(String username, String Password)
+        {
+            username = "abc";
+            Password = "xyz";
+        }
+           
+    }
+}
